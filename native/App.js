5f448341a6fcb5db8f8b7d0c4a2c629bd9725558
Luke Kaalim <lukekaalim@gmail.com>
2019-09-19 23:27:33 +1000

    Add more app

---
+++ native/App.js
@@ -0,0 +1,79 @@
+// @flow strict
+import React from 'react';
+import * as Font from 'expo-font';
+import { createHTTPClientFromFetch } from '@lukekaalim/http-client';
+import { createClient } from '@9now/client';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createNineNowComponents } from '@9now/components';
+
+const { useState, useEffect } = React;
+
+const primitives = {
+  Text: ({ style = {}, text = '' }) => {
+    const nativeStyle = {
+      fontFamily: 'hurme',
+      fontSize: style['font-size'],
+      color: style['color']
+    };
+    return <Text style={nativeStyle}>{text}</Text>
+  },
+  Box: ({ style = {}, children }) => {
+    const nativeStyle = {
+      ...style,
+    };
+    return <View style={nativeStyle}>{children}</View>
+  },
+  Image: ({ style = {}, source }) => {
+    const nativeStyle = {
+      width: style['width'],
+      height: style['height']
+    };
+    console.log(source);
+    return <Image style={nativeStyle} source={source}></Image>;
+  },
+};
+
+const { Card, Rail } = createNineNowComponents(primitives);
+
+const client = createClient('http://10.0.43.52:1243', createHTTPClientFromFetch(fetch, Headers))
+
+export default function App() {
+  const [homepageData, setHomePageData] = useState(null);
+  const [fontLoaded, setFontLoaded] = useState(false);
+  useEffect(() => {
+    const client = createClient('http://10.0.43.52:1243', createHTTPClientFromFetch(fetch, Headers));
+    client.getHomepage()
+      .then(homepage => setHomePageData(homepage))
+      .catch(error => console.error(error));
+  }, []);
+  console.log('RUNNGIN');
+  useEffect(() => {
+    Font.loadAsync({ hurme: require('./assets/fonts/Hurme.otf') }).then(() => setFontLoaded(true))
+  }, []);
+
+  if (fontLoaded && homepageData) {
+    console.log(fontLoaded)
+    return (
+      <View style={styles.container}>
+        {homepageData.rails.map(rail => (
+          <Rail key={JSON.stringify(rail.cards)} callToAction={rail.callToAction} cards={rail.cards} />
+        ))}
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Loading</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(17, 22, 25)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ server/src/index.js
@@ -0,0 +1,31 @@
+// @flow init
+const { createListener, createRoute, ok, notFound, internalServerError } = require('@lukekaalim/server');
+const { createServer } = require('http');
+const { readFile } = require('fs').promises;
+const { join } = require('path');
+const { homepageModel } = require('@9now/models');
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*'
+};
+
+const main = async () => {
+  const homepageRoute = createRoute('/home', 'GET', async () => {
+    try {
+      const fileContents = await readFile(join(__dirname, 'home.json'), 'utf-8');
+      const homepageResult = homepageModel.from(JSON.parse(fileContents));
+      if (homepageResult.type === 'success')
+        return ok(JSON.stringify(homepageResult.success), corsHeaders)
+      return internalServerError(JSON.stringify(homepageResult.failure.message), corsHeaders);
+    } catch (err) {
+      console.error(err);
+      return internalServerError(JSON.stringify(err.stack), corsHeaders);
+    }
+  });
+  const routes = [homepageRoute];
+  const listener = createListener(routes, () => notFound());
+  const server = createServer(listener);
+  server.listen(1243);
+};
+
+main();
