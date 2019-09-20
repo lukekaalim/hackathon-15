c8abfa8dc47b22a928fd86d69abf8510f3230c62
Luke Kaalim <lukekaalim@gmail.com>
2019-09-20 10:28:19 +1000

    compile the models library for max compat

---
+++ models/rollup.config.js
@@ -0,0 +1,24 @@
+const commonjs = require('rollup-plugin-commonjs');
+const resolve = require('rollup-plugin-node-resolve');
+const babel = require('rollup-plugin-babel');
+
+const path = require('path');
+
+module.exports = {
+  input: 'src/models',
+  output: [
+    {
+      file: 'dist/models.esm.js',
+      format: 'esm',
+    },
+    {
+      file: 'dist/models.cjs.js',
+      format: 'cjs',
+    }
+  ],
+  plugins: [
+    babel({ exclude: 'node_modules/**' }),
+    resolve(),
+    commonjs(),
+  ]
+}
